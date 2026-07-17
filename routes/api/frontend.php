34c8c3dd6a0f36b5726c05c7a3c758c8042128a0
Naresh Patel <30832955+nareshpatel007@users.noreply.github.com>
2026-07-17 10:50:32 +0530

    Update frontend.php

--- routes/api/frontend.php
+++ routes/api/frontend.php
@@ -8,14 +8,8 @@ use App\Http\Controllers\SubscriptionController;
 use App\Http\Controllers\ScrapingController;
 use App\Http\Controllers\OrderController;
 
-// Public auth
-Route::post('auth/callback/google', [AuthController::class, 'googleCallback']);
-Route::get('demo-call', [ScrapingController::class, 'demoCall']);
-Route::post('visitor/track', [\App\Http\Controllers\VisitorController::class, 'track']);
-Route::post('contact', [\App\Http\Controllers\ContactController::class, 'submitContact']);
-
 Route::middleware('verify.api.token')->group(function () {
-    // PCB Orders (Public)
+    // PCB Orders
     Route::prefix('orders/')->group(function () {
         Route::post('submit', [OrderController::class, 'store']);
     });
