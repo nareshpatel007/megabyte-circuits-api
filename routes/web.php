d2b6998ee99087f72013dc6132c6ec92820d01de
haresh <hdpatel234@gmail.com>
2026-08-27 14:31:55 +0530

    feat: add web routes for clearing application optimization caches

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Artisan;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/optimize-clear', function () {
+    Artisan::call('optimize:clear');
+    return response()->json([
+        'status' => true,
+        'message' => 'Optimization cache cleared successfully!',
+        'output' => Artisan::output()
+    ]);
+});
+
+Route::get('/clear-cache', function () {
+    Artisan::call('optimize:clear');
+    return response()->json([
+        'status' => true,
+        'message' => 'Optimization cache cleared successfully!',
+        'output' => Artisan::output()
+    ]);
+});
+
