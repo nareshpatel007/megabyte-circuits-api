5b565b50bf95b883d649daaae6764a4f271b7d23
haresh <hdpatel234@gmail.com>
2026-09-23 20:16:03 +0530

    feat: add FileUploadController to handle archive uploads and background processing

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -311,7 +311,11 @@ class FileUploadController extends Controller
                 }
 
                 // Fetch from Python service
+                $color = $request->query('color');
                 $targetUrl = rtrim($pythonUrl, '/') . '/' . ltrim($relPath, '/');
+                if ($color) {
+                    $targetUrl .= (str_contains($targetUrl, '?') ? '&' : '?') . 'color=' . urlencode($color);
+                }
                 $imgRes = Http::timeout(15)->get($targetUrl);
 
                 if ($imgRes->successful()) {
