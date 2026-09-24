ba50f6aae1b973b3a31d7fbf29d8391b50e40bab
haresh <hdpatel234@gmail.com>
2026-09-24 23:01:18 +0530

    feat: add EmailLogController for admin email logs management and statistics

--- app/Http/Controllers/Admin/EmailLogController.php
+++ app/Http/Controllers/Admin/EmailLogController.php
@@ -11,6 +11,7 @@ use App\Models\Credential;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
 
 class EmailLogController extends Controller
 {
